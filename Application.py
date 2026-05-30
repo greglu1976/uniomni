dc433169ba72a0e994b4a4826627aa7f14e88787
greglu1976 <purpleshiftter@yandex.ru>
2026-05-30 12:26:28 +0300

    v0.6.4

--- Application.py
+++ Application.py
@@ -107,7 +107,7 @@ class Application:
 
         Logger.set_container("log_content", "log_window")
         
-        dpg.create_viewport(title="Omni v0.6.3 16.05.26", width=1400, height=550)
+        dpg.create_viewport(title="Omni v0.6.4 30.05.26", width=1400, height=550)
         dpg.setup_dearpygui()
 
     def renew_abbrs_ru(self):
